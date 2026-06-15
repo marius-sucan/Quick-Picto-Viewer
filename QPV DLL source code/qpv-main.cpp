4a7d98329a46a09062f0c676044b79a08d0e5f4e
Marius <marius@users.noreply.github.com>
2026-06-15 03:40:38 -0700

    Optimize line drawing algorithms, reduce overdraws, fix bugs in array bounds and precision

--- QPV DLL source code/qpv-main.cpp
+++ QPV DLL source code/qpv-main.cpp
@@ -1142,7 +1142,7 @@ short inline testPointsOrientation(Point p, Point q, Point r) {
 // 1 -> Clockwise
 // 2 -> Counterclockwise
     float val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
-    if (val == 0)
+    if (fabs(val) < 1e-4)
        return 0;               // collinear
 
     return (val > 0) ? 1 : 2;  // clock or counterclockwise
@@ -1161,7 +1161,7 @@ bool findLinesIntersection(Point A, Point B, Point C, Point D, float &x, float &
     float c2 = a2 * (C.x) + b2 * (C.y);
 
     float determinant = a1 * b2 - a2 * b1;
-    if (determinant == 0)
+    if (fabs(determinant) < 1e-4)
        return 0; // The lines are parallel
 
     x = (b2 * c1 - b1 * c2) / determinant;
@@ -1219,44 +1219,75 @@ void drawLineSegmentMask(int x0, int y0, int x1, int y1, const bool &p, const in
    const int dy = -abs(y1-y0), sy = (y0<y1) ? 1 : -1;
    int err = dx + dy, e2, gx, gy;
    auto &currentGrid = (roundedJoins==1) ? DrawLineCapsGrid : lineGrid;
-   const int kl = currentGrid.size() - 15;
+   const int kl = max(0, (int)currentGrid.size() - 15);
    const int kr = 15;
-   for (;;) {
-      int loops = 0;
-      for (auto &point : currentGrid)
-      {
-          bool okay = 1;
-          if (roundedJoins!=1 && rectu!=NULL)
-          {
-             loops++;
-             if (loops<kr || loops>kl) {
-                okay = isPointInPolygon(x0 + point.first, y0 + point.second, rectu, 4);
-             } else {
-                okay = 1;
-             }
-          }
+   bool checkPoly = (roundedJoins!=1 && rectu!=NULL);
 
-          if (okay==1)
+   if (clipMode != 2) {
+       for (;;) {
+          int loops = 0;
+          for (auto &point : currentGrid)
           {
-              gx = x0 + point.first - polyX;
-              gy = y0 + point.second - polyY + offsetY;
-              if (gy>=0 && gy<polyH && gx>=0 && gx<polyW)
+              bool okay = 1;
+              if (checkPoly)
               {
-                 if (clipMode!=2)
-                    okay = isPointInOtherMask(gx, gy, clipMode);
+                 loops++;
+                 if (loops<kr || loops>kl) {
+                    okay = isPointInPolygon(x0 + point.first, y0 + point.second, rectu, 4);
+                 }
+              }
 
-                 if (okay==1)
-                    polygonMaskMap[(INT64)gy * polyW + gx] = p;
+              if (okay)
+              {
+                  gx = x0 + point.first - polyX;
+                  gy = y0 + point.second - polyY + offsetY;
+                  if (gy>=0 && gy<polyH && gx>=0 && gx<polyW)
+                  {
+                     if (isPointInOtherMask(gx, gy, clipMode))
+                        polygonMaskMap[(INT64)gy * polyW + gx] = p;
+                  }
               }
           }
-      }
 
-      // drawLineCapOnMask(x0, y0);
-      if (x0 == x1 && y0 == y1) break;
+          // drawLineCapOnMask(x0, y0);
+          if (x0 == x1 && y0 == y1) break;
 
-      e2 = 2*err;
-      if (e2 >= dy) { err += dy; x0 += sx; }                        /* x step */
-      if (e2 <= dx) { err += dx; y0 += sy; }                        /* y step */
+          e2 = 2*err;
+          if (e2 >= dy) { err += dy; x0 += sx; }                        /* x step */
+          if (e2 <= dx) { err += dx; y0 += sy; }                        /* y step */
+       }
+   } else {
+       for (;;) {
+          int loops = 0;
+          for (auto &point : currentGrid)
+          {
+              bool okay = 1;
+              if (checkPoly)
+              {
+                 loops++;
+                 if (loops<kr || loops>kl) {
+                    okay = isPointInPolygon(x0 + point.first, y0 + point.second, rectu, 4);
+                 }
+              }
+
+              if (okay)
+              {
+                  gx = x0 + point.first - polyX;
+                  gy = y0 + point.second - polyY + offsetY;
+                  if (gy>=0 && gy<polyH && gx>=0 && gx<polyW)
+                  {
+                     polygonMaskMap[(INT64)gy * polyW + gx] = p;
+                  }
+              }
+          }
+
+          // drawLineCapOnMask(x0, y0);
+          if (x0 == x1 && y0 == y1) break;
+
+          e2 = 2*err;
+          if (e2 >= dy) { err += dy; x0 += sx; }                        /* x step */
+          if (e2 <= dx) { err += dx; y0 += sy; }                        /* y step */
+       }
    }
 }
 
@@ -1266,12 +1297,19 @@ void prepareTranslatedLineSegments(const float &thickness, vector<double> &offse
    {
        const int i = pts*2;
        Point a = {PointsList[i], PointsList[i + 1]};
-       Point b = {PointsList[i + 2], PointsList[i + 3]};
-       Point c = {PointsList[i + 4], PointsList[i + 5]};   // it is meant as a backup, if A and B are too close, the AC segment will be used
+       Point b, c;
        if (pts==pci)
        {
           b = {PointsList[0], PointsList[1]};
           c = {PointsList[2], PointsList[3]};
+       } else if (pts == pci - 1)
+       {
+          b = {PointsList[i + 2], PointsList[i + 3]};
+          c = {PointsList[0], PointsList[1]};
+       } else
+       {
+          b = {PointsList[i + 2], PointsList[i + 3]};
+          c = {PointsList[i + 4], PointsList[i + 5]};
        }
 
       double dx = b.x - a.x;
@@ -1312,18 +1350,28 @@ void prepareTranslatedLineSegments(const float &thickness, vector<double> &offse
 }
 
 void stampCircleMaskAt(const int &dx, const int &dy, const int &tt, const int &rr, const int &offsetY, const int &clipMode, const bool &fillMode) {
-      for (auto &point : DrawLineCapsGrid)
-      {
-          bool okay = 1;
-          int gx = dx + point.first - polyX;
-          int gy = dy + point.second - polyY + offsetY;
-          if (gy>=0 && gy<polyH && gx>=0 && gx<polyW)
+      int bx = dx - polyX;
+      int by = dy - polyY + offsetY;
+      if (clipMode != 2) {
+          for (auto &point : DrawLineCapsGrid)
           {
-             if (clipMode!=2)
-                okay = isPointInOtherMask(gx, gy, clipMode);
-
-             if (okay==1)
-                polygonMaskMap[(INT64)gy * polyW + gx] = fillMode;
+              int gx = bx + point.first;
+              int gy = by + point.second;
+              if (gy>=0 && gy<polyH && gx>=0 && gx<polyW)
+              {
+                 if (isPointInOtherMask(gx, gy, clipMode))
+                    polygonMaskMap[(INT64)gy * polyW + gx] = fillMode;
+              }
+          }
+      } else {
+          for (auto &point : DrawLineCapsGrid)
+          {
+              int gx = bx + point.first;
+              int gy = by + point.second;
+              if (gy>=0 && gy<polyH && gx>=0 && gx<polyW)
+              {
+                 polygonMaskMap[(INT64)gy * polyW + gx] = fillMode;
+              }
           }
       }
 }
@@ -1372,8 +1420,7 @@ DLL_API int DLL_CALLCONV drawLineAllSegmentsMask(float* PointsList, int PointsCo
         int i = pts*2;
         float xa = PointsList[i];
         float ya = PointsList[i + 1];
-        float xb = PointsList[i + 2];
-        float yb = PointsList[i + 3];
+        float xb, yb;
         if (pts==pci)
         {
            if (closed!=1)
@@ -1381,17 +1428,46 @@ DLL_API int DLL_CALLCONV drawLineAllSegmentsMask(float* PointsList, int PointsCo
 
            xb = PointsList[0];
            yb = PointsList[1];
+        } else {
+           xb = PointsList[i + 2];
+           yb = PointsList[i + 3];
         }
 
         if (roundedJoins==1)
         {
-           drawLineSegmentMask(xa, ya, xb, yb, fillMode, offsetY, roundedJoins, NULL, clipMode);
+           Point npA = {xa, ya};
+           Point npB = {xb, yb};
+           Point np1, np2, np3, np4;
+           const double dx = npB.x - npA.x;
+           const double dy = npB.y - npA.y;
+           if (fabs(dx) < 0.01 && fabs(dy) < 0.01) {
+               stampCircleMaskAt(xa, ya, thickness, pow(thickness, 2), offsetY, clipMode, fillMode);
+           } else {
+               translateLine(npA, npB, dx, dy, thickness, np1, np2, np3, np4);
+
+               float* dynamicArray = new float[8];
+               dynamicArray[0] = np1.x; // zxa
+               dynamicArray[1] = np1.y; // zya
+               dynamicArray[2] = np3.x; // zxb
+               dynamicArray[3] = np3.y; // zyb
+               dynamicArray[4] = np4.x; // zdxb
+               dynamicArray[5] = np4.y; // zdyb
+               dynamicArray[6] = np2.x; // zdxa
+               dynamicArray[7] = np2.y; // zdya
+               
+               drawLineSegmentMask(np1.x, np1.y, np2.x, np2.y, fillMode, offsetY, 0, dynamicArray, clipMode); 
+               delete[] dynamicArray;
+               
+               stampCircleMaskAt(xa, ya, thickness, pow(thickness, 2), offsetY, clipMode, fillMode);
+               stampCircleMaskAt(xb, yb, thickness, pow(thickness, 2), offsetY, clipMode, fillMode);
+           }
         } else
         {
            Point npA, npB, np1, np2, np3, np4;
            extendLine({xa, ya}, {xb, yb}, 1.0f, npA, npB);
            const double dx = npB.x - npA.x;
            const double dy = npB.y - npA.y;
+           if (fabs(dx) < 0.01 && fabs(dy) < 0.01) continue;
            translateLine(npA, npB, dx, dy, thickness, np1, np2, np3, np4);
 
            float* dynamicArray = new float[8];
